a3f6692e851dd7e8d1144c2bc052422de65a0f6e
vishal kumar <vishaljpkumar4970@gmail.com>
2026-03-11 18:04:04 +0530

    new com3

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,7 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", cors());
+
 
 // Database connection
 dbConect()
