4eb4c5577e6a06c0468c7b0667551d7e7d7a555d
Babloothk08 <babloothakur42021@gmail.com>
2026-03-11 18:05:59 +0530

    pull

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", cors());
+app.options("/*", cors());
 
 // Database connection
 dbConect()
